942e1b769b736329b84cc17dcae911dc69c2fb94
Ayush Kumar <aykr571@gmail.com>
2024-01-05 21:58:38 +0530

    fF

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -28,7 +28,7 @@ const CartScreen = () => {
         Shopping Cart
       </h3>
       {cartItems.length === 0 ? (
-        <h2>cart is empty</h2>
+        <h2 className="text-3xl font-bold text-center mt-8">Cart is empty😞</h2>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
