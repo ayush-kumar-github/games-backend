8e28e38398a8372f4fb7e619f977ab0416c9d299
Ayush Kumar <aykr571@gmail.com>
2024-01-05 21:44:13 +0530

    fF

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -29,43 +29,49 @@ const CartScreen = () => {
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div>
-          {cartItems.map((product) => (
-            <div key={product._id} className="flex flex-col p-4 mb-4 md:mb-0">
-              <img
-                alt="product"
-                className="w-[40%] md:w-[30%] h-auto mb-2 md:mb-0"
-                src={product.imageUrl}
-              />
-              <div className="flex flex-col md:ml-4">
-                <h2 className="text-lg md:text-xl font-bold">{product.name}</h2>
-                <p className="text-md md:text-lg">
-                  {product.qty} x ${product.price} = $
-                  {(product.qty * product.price).toFixed(2)}
-                </p>
-              </div>
-              <div className="flex items-center mt-2 md:mt-0 md:ml-4">
-                <select
-                  className="bg-gray-200 w-1/2 md:w-full"
-                  value={product.qty}
-                  onChange={(e) =>
-                    addtoCartHandler(product, Number(e.target.value))
-                  }
-                >
-                  {[...Array(product.countInStock).keys()].map((x) => (
-                    <option key={x + 1} value={x + 1}>
-                      {x + 1}
-                    </option>
-                  ))}
-                </select>
-                <div
-                  className="ml-2 cursor-pointer"
-                  onClick={() => removeFromCartHandler(product._id)}
-                >
-                  🗑️
+          {cartItems === 0 ? (
+            <h2>cart is empty</h2>
+          ) : (
+            cartItems.map((product) => (
+              <div key={product._id} className="flex flex-col p-4 mb-4 md:mb-0">
+                <img
+                  alt="product"
+                  className="w-[40%] md:w-[30%] h-auto mb-2 md:mb-0"
+                  src={product.imageUrl}
+                />
+                <div className="flex flex-col md:ml-4">
+                  <h2 className="text-lg md:text-xl font-bold">
+                    {product.name}
+                  </h2>
+                  <p className="text-md md:text-lg">
+                    {product.qty} x ${product.price} = $
+                    {(product.qty * product.price).toFixed(2)}
+                  </p>
+                </div>
+                <div className="flex items-center mt-2 md:mt-0 md:ml-4">
+                  <select
+                    className="bg-gray-200 w-1/2 md:w-full"
+                    value={product.qty}
+                    onChange={(e) =>
+                      addtoCartHandler(product, Number(e.target.value))
+                    }
+                  >
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                  </select>
+                  <div
+                    className="ml-2 cursor-pointer"
+                    onClick={() => removeFromCartHandler(product._id)}
+                  >
+                    🗑️
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
         <div>
           <div className="border-t pt-4">
