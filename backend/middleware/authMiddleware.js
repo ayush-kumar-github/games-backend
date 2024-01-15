38888d6fe8e1202fc1dddcb4df06f39bb99ddf2c
Ayush Kumar <aykr571@gmail.com>
2024-01-15 10:33:21 +0530

    GGG

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -14,7 +14,6 @@ const protect = asyncHandler(async (req, res, next) => {
 
       next();
     } catch (error) {
-      console.error(error);
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
