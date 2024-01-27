bbd66057521f4bfdfac63fb21f31db5e5f933509
Ayush Kumar <aykr571@gmail.com>
2024-01-27 09:42:26 +0530

    HH

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(
-  cors()
-  // cors({
-  //   origin: "https://gamesbyayush.vercel.app/",
-  //   optionsSuccessStatus: 200,
-  //   credentials: true,
-  // })
+  cors({
+    origin: "https://gamesbyayush.vercel.app/",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
 );
 
 app.use("/api/products", productRoutes);
