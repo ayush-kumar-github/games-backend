7815e6df717421877935bd7347b9cb3261af8edf
Ayush Kumar <aykr571@gmail.com>
2024-01-28 11:18:04 +0530

    HHH

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    domain: ".vercel.app",
+    // domain: "localhost",
 
     // sameSite: "None",
   });
