711a9115f9e623d41ea81a29365cc1827b3d06ad
Ayush Kumar <aykr571@gmail.com>
2024-01-28 11:26:20 +0530

    HHH

--- backend/server.js
+++ backend/server.js
@@ -22,17 +22,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  // cors({
-  //   origin: "http://localhost:3000",
-  //   credentials: true,
-  // })
-  cors({
-    origin: "https://gamesbyayush.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   // cors({
+//   //   origin: "http://localhost:3000",
+//   //   credentials: true,
+//   // })
+//   cors({
+//     origin: "https://gamesbyayush.vercel.app",
+//     credentials: true,
+//   })
+// );
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://gamesbyayush.vercel.app",
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
